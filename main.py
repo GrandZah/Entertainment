00c0b118423dbf033f60c96b3384dab860c98e46
zaits <zaitsev.zahar2016@yandex.ru>
2025-01-05 20:52:12 +0300

    v0.1.Simple_working_version

---
+++ main.py
@@ -0,0 +1,21 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from config import BOT_TOKEN
+
+# Обработчик команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('HI! Я ваш бот. Чем могу помочь?')
+
+# Главная функция для запуска бота
+def main():
+    # Создаем объект Application и передаем токен
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Регистрируем обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
